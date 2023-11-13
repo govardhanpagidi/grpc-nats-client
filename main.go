ac3e07867444003eaa507e8188f4d235079a5451
govardhan <govardhan.pagidi@mongodb.com>
2023-11-13 12:00:58 -0500

    jet stream client added

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/golang/protobuf/proto"
 	pb "github.com/govardhanpagidi/nats-client/fxconversion"
@@ -34,8 +35,67 @@ func main() {
 	// Subject for gRPC messages
 	subject := "FX_DETERMINATION"
 
+	//err = sendMessageToNats(nc, subject)
+	err = sendMessageToNatsJetStream(nc, subject)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func sendMessageToNatsJetStream(nc *nats.Conn, subject string) error {
+	js, err := nc.JetStream(nats.PublishAsyncMaxPending(256))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Marshal your Protocol Buffer message
+	message := ConversionReq{
+		TenantID:       12347,
+		BankID:         123456,
+		BaseCurrency:   "GBP",
+		TargetCurrency: "USD",
+		Tier:           "2",
+		Amount:         1000,
+	}
+	msgByteArray, err := json.Marshal(&message)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Publish gRPC message to NATS subject
+	_, err = js.Publish(subject, msgByteArray)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Published message to nats jetstream:", string(msgByteArray))
+	fmt.Println(" subject:", subject)
+
+	//// This code is optional and only needed when you want to listen to a response
+	//_, err = nc.Subscribe("FX_CONVERTED", func(msg *nats.Msg) {
+	//	fmt.Printf("Received a message: %s\n", string(msg.Data))
+	//
+	//	var response pb.ConversionResponse
+	//	err := proto.Unmarshal(msg.Data, &response)
+	//	if err != nil {
+	//		log.Printf("Error unmarshalling gRPC message: %v\n", err)
+	//		return
+	//	}
+	//
+	//	fmt.Printf("Received a message: %v\n", response)
+	//
+	//})
+	//
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+
+	return nil
+}
+
+func sendMessageToNats(nc *nats.Conn, subject string) error {
 	yourMessage := &pb.ConversionRequest{
-		TenantID:       12345,
+		TenantID:       12347,
 		BankID:         123456,
 		BaseCurrency:   "GBP",
 		TargetCurrency: "USD",
@@ -57,7 +117,6 @@ func main() {
 	fmt.Println("Published gRPC message to NATS subject:", subject)
 
 	// This code is optional and only needed when you want to listen to a response
-
 	_, err = nc.Subscribe("FX_CONVERTED", func(msg *nats.Msg) {
 		fmt.Printf("Received a message: %s\n", string(msg.Data))
 
@@ -67,9 +126,7 @@ func main() {
 			log.Printf("Error unmarshalling gRPC message: %v\n", err)
 			return
 		}
-
 		fmt.Printf("Received a message: %v\n", response)
-
 	})
 
 	if err != nil {
